8687638dd4e30f47538a63eb03ad3f99d191c843
Pavel Larin <www.larinpavel@mail.ru>
2023-04-13 23:55:47 +0500

    Added comment explanations to ReadAsString

--- src/IoAndFileSystem/CharStreams/ReadAsString.java
+++ src/IoAndFileSystem/CharStreams/ReadAsString.java
@@ -20,19 +20,18 @@ import java.nio.charset.Charset;
 
 public class ReadAsString {
 
-
-
-    // TODO Explain all
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        // Create a new ByteArrayOutputStream to hold the data read from the InputStream
         ByteArrayOutputStream result = new ByteArrayOutputStream();
+        // Create a bugger to hold the data read from the InputStream
         byte[] buffer = new byte[1024];
         int length;
+        // Loop through the InputStream, reading data into the buffer
         while ((length = inputStream.read(buffer)) != -1) {
+            // Write the data from the buffer to the ByteArrayOutputStream
             result.write(buffer, 0, length);
         }
+        // Convert the data in the ByteArrayOutputStream
         return new String(result.toByteArray(), charset);
-
     }
-
-
 }
